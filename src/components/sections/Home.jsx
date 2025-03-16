d2c811e3d2b8e473f6b131f8616cf25380af568f
Issa Abu <168141352+Blackscorche@users.noreply.github.com>
2025-03-16 01:31:48 +0100

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -50,7 +50,7 @@ function Home() {
           {/* Buttons */}
           <div className="flex justify-center space-x-3">
             <a 
-              href="Projects" 
+              href="#projects" 
               className='bg-blue-500 py-3 px-5 rounded font-medium transition duration-200 
                          relative overflow-hidden hover:-translate-y-0.5 
                          hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]'
@@ -59,7 +59,7 @@ function Home() {
             </a>
 
             <a 
-              href="contact" 
+              href="#contact" 
               className='border border-blue-500/50 text-blue-500 py-3 px-5 rounded font-medium 
                          transition-all duration-200 relative overflow-hidden 
                          hover:-translate-y-0.5 hover:bg-blue-500/10'
